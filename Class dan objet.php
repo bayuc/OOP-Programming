6cb3dd47f31612f0c4eb326080f11e4865638f47
Bayu C.W.R <39794195+bayuc@users.noreply.github.com>
2023-11-20 11:01:57 +0700

    Add files via upload

---
+++ Class dan objet.php
@@ -0,0 +1,18 @@
+<?php   
+
+// Penjualan Mobil
+// Toyota 
+// BMW
+
+class Mobil{
+
+    public $Tahun_produksi = 0,
+           $Jenis = "jenis",
+           $Warna = "warna" ,
+           $Harga = 0;
+
+}
+
+$Mobil1 = new Mobil();
+
+var_dump($Mobil1);
